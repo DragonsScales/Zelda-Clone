228b2154cbfeaaf58bdca31ac422c214aff9ed14
DragonsScales <89547899+DragonsScales@users.noreply.github.com>
2023-02-14 10:38:48 -0700

    Video 8 done

--- Tiny Adventure/Player/Player.gd
+++ Tiny Adventure/Player/Player.gd
@@ -1,9 +1,7 @@
 extends KinematicBody2D
 
-
 export (int) var speed = 80
 var attacking = false
-
 
 func _physics_process(delta):
 	check_input()
@@ -35,15 +33,19 @@ func check_input():
 			show_Sprite("Walk")
 			$AnimationTree.set("parameters/Idle/blend_position", input_vector)
 			$AnimationTree.set("parameters/Walk/blend_position", input_vector)
+			$AnimationTree.set("parameters/Attack/blend_position", input_vector)
+			
 			move_and_slide(input_vector*speed)
 
 	if Input.is_action_just_pressed("Attack"):
 		attacking = true
 		show_Sprite("Attack")
-		get_node("AnimationPlayer").play("Attack Down")
+		$AnimationTree.get("parameters/playback").travel("Attack")
 
+func Attack_finished():
+	attacking = false
 
-func _on_AnimationPlayer_animation_finished(anim_name):
-	if "Attack" in anim_name:
-		attacking = false
-	
+func _on_Attack_Detector_body_exited(body):
+	print(body.name)
+	if "Hostile" in body.name:
+		body.health -= 3
--- Tiny Adventure/World Objects/Monsters/HostileAI.gd
+++ Tiny Adventure/World Objects/Monsters/HostileAI.gd
@@ -3,12 +3,14 @@ extends KinematicBody2D
 var speed = 30
 var see_player = false
 var direction = Vector2()
+var health = 5
 
 func _physics_process(delta):
-	
+	if health <= 0:
+		queue_free()
+	#he adds this in, but it doesn't actually matter
+	direction = Vector2.ZERO;
 	if see_player == true:
-		#he adds this in, but it doesn't actually matter
-		direction = Vector2.ZERO;
 		
 		var player = get_node("../../Friendly/Player")
 		direction = (player.position - position).normalized()
@@ -22,8 +24,6 @@ func _physics_process(delta):
 func _on_Player_Check_body_entered(body):
 	if body.name == "Player":
 		see_player = true
-
-
 
 func _on_Player_Check_body_exited(body):
 	if body.name == "Player":
